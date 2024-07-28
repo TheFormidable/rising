7a709928300f72609856d224c5592928ae4775be
ISAAC <jisaacvega@gmail.com>
2024-07-28 17:33:41 -0500

    Convert device tree from LineageOS to RisingOS Fourteen for duchamp on version fourteen

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/rising_duchamp.mk
+
+COMMON_LUNCH_CHOICES := \
+    rising_duchamp-user \
+    rising_duchamp-userdebug \
+    rising_duchamp-eng
